ff5a40b13f3113334bb24ec175256c98440593bb
aqdasghani <70949084+aqdasghani@users.noreply.github.com>
2025-12-09 12:49:22 +0530

    Responsive fixes in services-section

--- components/Servicesection.js
+++ components/Servicesection.js
@@ -33,14 +33,16 @@ const Servicesection = () => {
                 <h1 className='text-5xl font-bold py-5'>My Services</h1>
                 <p className='text-xl px-5' >I offer a comprehensive range of web development services to help bring your ideas to life.</p>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 max-w-6xl mx-auto p-6">
+           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 max-w-6xl mx-auto p-6">
     {services.map((service, index) => (
         <TiltCard
             key={index}
             className="bg-white w-full min-h-[350px] border rounded-2xl p-6 text-black shadow-md"
-            highlightClassName="hidden sm:block">
+            highlightClassName="hidden sm:block bg-white/15 dark:bg-white/20"
+        >
             <div>
                 <div className="text-3xl mb-4">{service.icon}</div>
+
                 <h3 className="text-xl font-semibold mb-2">
                     {service.title}
                 </h3>
@@ -49,6 +51,7 @@ const Servicesection = () => {
                     {service.desc}
                 </p>
 
+        
                 <ul className="list-disc list-inside space-y-1 pl-2">
                     {service.pointers.map((pointer, i) => (
                         <li key={i}>{pointer}</li>
@@ -58,6 +61,7 @@ const Servicesection = () => {
         </TiltCard>
     ))}
 </div>
+
 
         </section>
 
